28aedadade68a962ec385bb6231e19f2dc483066
RCPisAwesome <RCPisAwesome@gmail.com>
2020-04-15 20:49:07 +0100

    Replaced Commands With A UI

--- RCPMenu.lua
+++ RCPMenu.lua
@@ -1,114 +1,47 @@
 empty = true
 allowthird = false
 allowthirdtoggle = true
-enabled = true
 CreateThread( function()
 	while true do
 		Wait(0)
-		if enabled then
-			if (GetFollowPedCamViewMode() == 4 and empty) then
+		if (GetFollowPedCamViewMode() == 4 and empty) then
+        	SendNUIMessage({ShowUI = true})
+        	empty = false
+        elseif not (GetFollowPedCamViewMode() == 4) then
+        	if (allowthird and allowthirdtoggle) then
         		SendNUIMessage({ShowUI = true})
-        		empty = false
-        	elseif not (GetFollowPedCamViewMode() == 4) then
-        		if (allowthird and allowthirdtoggle) then
-        			SendNUIMessage({ShowUI = true})
-        			allowthirdtoggle = false
-        		elseif not allowthird then
-        			SendNUIMessage({ShowUI = false})
-        			allowthirdtoggle = true
-        		end
-        		empty = true
+        		allowthirdtoggle = false
+        	elseif not allowthird then
+        		SendNUIMessage({ShowUI = false})
+        		allowthirdtoggle = true
         	end
+        	empty = true
         end
 	end
 end)
 
-RegisterCommand('rcpaxonvolume', function(source,volume)
-	vol = tonumber(volume[1])
-	if ((0 < vol) and (vol <= 1.0)) then
-		SendNUIMessage({ShowUI = true, Volume = vol})
-		datatext = "Volume Set To: "..vol
-	else
-		datatext = "Invalid Volume! Minimum volume is 0.01 and maximum volume is 1.0"
-	end
-	DrawTextWait()
-end, false)
+RegisterNUICallback('togglethirdperson', function()
+	allowthird = not allowthird
+end)
 
-RegisterCommand('rcpaxontimeoffset', function(source,offset)
-	if tostring(offset[1]) == "utc" then
-		SendNUIMessage({ShowUI = true, UTC = true})
-		datatext = "Time Set To UTC"
-	else
-		if tonumber(offset[1]) ~= nil then
-			setoffset = tonumber(offset[1])
-			if ((-12 <= setoffset) and (setoffset <= 12)) then
-				SendNUIMessage({ShowUI = true, Offsetsend = true, Offset = setoffset})
-				datatext = "Time Offset Set To: "..setoffset
-			else
-				datatext3 = "Invalid Offset! Minimum is -12 & maximum is +12"
-				datatext2 = "This is your time zone without daylight savings compared to UTC time"
-				datatext = "Do /rcpaxontimeoffset utc to reset back to utc"
-			end
-		else
-			datatext3 = "Invalid Offset! Minimum is -12 & maximum is +12"
-			datatext2 = "This is your time zone without daylight savings compared to UTC time"
-			datatext = "Do /rcpaxontimeoffset utc to reset back to utc"
-		end
-	end
-	DrawTextWait()
-	datatext3 = ""
-	datatext2 = ""
-end, false)
-
-local background = 0
-RegisterCommand('rcpaxonbackground', function()
-	if background == 0 then
-		datatext = "Background Box Enabled"
-		SendNUIMessage({ShowUI = true, ShowBox = true})
-		background = 1
-	elseif background == 1 then
-		datatext = "Background Box Disabled"
-		SendNUIMessage({ShowUI = true, ShowBox = false})
-		background = 0
-	end
-	DrawTextWait()
-end, false)
-
-local thirdperson = 0
-RegisterCommand('rcpaxonallowthirdperson', function()
-	if thirdperson == 0 then
-		datatext = "UI shows when in third person"
-		allowthird = true
-		thirdperson = 1
-	elseif thirdperson == 1 then
-		datatext = "UI doesn't show when in third person"
-		allowthird = false
-		thirdperson = 0
-	end
-	DrawTextWait()
-end, false)
-
-local show = 0
 RegisterCommand('rcpaxon', function()
-	if show == 0 then
-		datatext = "Disabled"
-		enabled = false
-		SendNUIMessage({ShowUI = false})
-		show = 1
-	elseif show == 1 then
-		datatext = "Enabled"
-		enabled = true
-		SendNUIMessage({ShowUI = true})
-		show = 0
+	if GetFollowPedCamViewMode() == 4 then
+		SendNUIMessage({ShowPowerButton = true})
+		SetNuiFocus(true,true)
+	else
+		DrawTextWait()
 	end
-	DrawTextWait()
 end, false)
+
+RegisterNUICallback('close', function()
+	SetNuiFocus(false)	
+end)
 
 function DrawText(text,x,y)
 	SetTextFont(0)
 	SetTextProportional(0)
 	SetTextScale(0.35,0.35)
-	SetTextColour(255,255,255,255)--r,g,b,a
+	SetTextColour(255,0,0,255)--r,g,b,a
 	SetTextCentre(true)--true,false
 	SetTextDropshadow(1,0,0,0,200)--distance,r,g,b,a
 	SetTextEdge(1, 0, 0, 0, 255)
@@ -118,19 +51,16 @@ function DrawText(text,x,y)
 end
 
 function DrawTextWait()
-	toggletext = not toggletext
+	toggletext = true
 	Wait(3000)
-	toggletext = not toggletext
+	toggletext = false
 end
-datatext3 = ""
-datatext2 = ""
-CreateThread( function()
+
+CreateThread(function()
 	while true do
 		Wait(0)
 		if toggletext then
-			DrawText(tostring(datatext),0.5,0.92)
-			DrawText(tostring(datatext2),0.5,0.90)
-			DrawText(tostring(datatext3),0.5,0.88)
+			DrawText("You Need To Be In First Person",0.5,0.92)
 		end
 	end
 end)
--- README.md
+++ README.md
@@ -2,19 +2,22 @@
 Axon body camera overlay for FiveM, the multiplayer mod for GTAV!
 
 # Screenshots:
-<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxon/1.jpg" target="_blank">**rcpaxon**</a>|<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxon/2.jpg" target="_blank">**rcpaxonbackground**</a>|<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxon/3.jpg" target="_blank">**rcpaxontimeoffset**</a>|<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxon/4.jpg" target="_blank">**rcpaxonallowthirdperson**</a>
-:---:|:---:|:---:|:---:
-<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxon/1.jpg" target="_blank"><img alt="FiveMRCPAxon" src="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxon/1.jpg"></a>|<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxon/2.jpg" target="_blank"><img alt="FiveMRCPAxon" src="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxon/2.jpg"></a>|<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxon/3.jpg" target="_blank"><img alt="FiveMRCPAxon" src="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxon/3.jpg"></a>|<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxon/4.jpg" target="_blank"><img alt="FiveMRCPAxon" src="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxon/4.jpg"></a>
+<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxonCamera/1.jpg" target="_blank">**/rcpaxon**</a>|<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxonCamera/2.jpg" target="_blank">**Toggle Power**</a>|<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxonCamera/3.jpg" target="_blank">**Time Offset**</a>
+<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxonCamera/4.jpg" target="_blank">**Volume**</a>|<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxonCamera/5.jpg" target="_blank">**Toggle Background**</a>|<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxonCamera/6.jpg" target="_blank">**Toggle Third Person**</a>
+:---:|:---:|:---:
+<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxonCamera/1.jpg" target="_blank"><img alt="FiveMRCPAxonCamera" src="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxonCamera/1.jpg"></a>|<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxonCamera/2.jpg" target="_blank"><img alt="FiveMRCPAxonCamera" src="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxonCamera/2.jpg"></a>|<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxonCamera/3.jpg" target="_blank"><img alt="FiveMRCPAxonCamera" src="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxonCamera/3.jpg"></a>
+<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxonCamera/4.jpg" target="_blank"><img alt="FiveMRCPAxonCamera" src="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxonCamera/4.jpg"></a>|<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxonCamera/5.jpg" target="_blank"><img alt="FiveMRCPAxonCamera" src="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxonCamera/5.jpg"></a>|<a href="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxonCamera/6.jpg" target="_blank"><img alt="FiveMRCPAxonCamera" src="https://www.rcpisawesome.co.uk/dev/FiveMRCPAxonCamera/6.jpg"></a>
+
 # Video:
 coming soon
 # Patreon:
 [![patreon](https://c5.patreon.com/external/favicon/favicon.ico)](https://www.patreon.com/RCPisAwesome)     For early access to scripts and videos and other rewards, support me on my Patreon page!
 # Features:
-- Doing "/rcpaxon" in the Fivem chat toggles everything to be shown or hidden
-- Doing "/rcpaxonbackground" in the Fivem chat toggles a box behind the text and logo
-- Doing "/rcpaxonallowthirdperson" in the Fivem chat toggles if the UI shows in all views or first person only
-- Doing "/rcpaxonvolume" in the Fivem chat changes the loudness of the beeps, minimum is 0.01 and maximum is 1.0
-- Doing "/rcpaxontimeoffset" in the Fivem chat changes the offset in military hours compared to UTC time without daylight savings. Minimum is -12 and maximum is +12. Doing "/rcpaxontimeoffset utc" resets it back to UTC
+- Doing "/rcpaxon" in the Fivem chat when in first person bring up the power button to turn on the overlay
+- Can toggle a box behind the text and logo for easier reading
+- UI can work in all views or in first person
+- Changeable beep volume
+- Changeable time offset compared to UTC time
 
 For more information on why Axon use UTC time [check out their website](https://help.axon.com/hc/en-us/articles/115002746247-Axon-Camera-Video-Watermark-Timestamp) and use this [wikipedia page](https://en.wikipedia.org/wiki/List_of_UTC_time_offsets) to find what your UTC offset would be if you want to use one.
 # Installation:
